122de6c9c38ca9f2d3335c467bdc2291c5e273f2
Leonardas SInkevicius <141874292+LimeFizzy@users.noreply.github.com>
2023-11-01 16:26:09 +0200

    Create ltulib.h
    Library file with main C keywords is created

---
+++ ltulib.h
@@ -0,0 +1,49 @@
+#ifndef _LTULIB_H_
+#define _LTULIB_H_
+
+//--------------Data types-----------------
+#define sveikas int
+#define ilgas long
+#define trumpas short
+#define dvigubas double
+#define pluduriojantis float
+#define simbolis char
+#define su_zenklu signed
+#define be_zenklo unsigned
+#define numeruota enum
+#define tuscias void
+
+//--------------Control flow---------------
+#define stabdyk break
+#define toliau continue
+#define daryk do
+#define pakolei while
+#define kol for
+#define jeigu if
+#define arba else if
+#define kitais_atvejais else
+#define eik goto
+#define kaitaliok switch
+#define grazink return
+
+////-----------Storage classes--------------
+#define automatiskai auto
+#define konstanta const
+#define iskelti extern
+#define registruok register
+#define statinis static
+
+////-------Function and program flow--------
+#define atvejis case
+#define iprasta default
+#define dydis sizeof
+#define pakeist typedef
+
+////--------Structures and unions-----------
+#define struktura struct
+#define sajunga union
+
+////---------------Other--------------------
+#define nepastovus volatile
+
+#endif
